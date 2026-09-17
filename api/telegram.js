9641bbace7a39f1505305056463019c1375bdab1
chikaraogita1971 <168136286+chikaraogita1971@users.noreply.github.com>
2026-09-18 01:34:56 +0900

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,4 +1,3 @@
-```javascript
 module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(200).send("OK");
@@ -77,5 +76,4 @@ module.exports = async function handler(req, res) {
     return res.status(200).send("OK");
   }
 };
-```
 
